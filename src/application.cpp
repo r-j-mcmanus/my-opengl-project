8412d672bee197b3ea2a20dae772081accd534fd
Ryan Mcmanus <35876565+r-j-mcmanus@users.noreply.github.com>
2023-12-31 13:17:33 +0000

    vertex arrays in openGL

--- src/application.cpp
+++ src/application.cpp
@@ -176,21 +176,32 @@ int main(void)
         2, 3, 0
     };
 
+
+
+    // make the vertex array object that will link the array buffer
+    // we make the vao with the id we have then bind the newly made vao
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao))
+
+    //we make all the buffers we need and bind to a vertex array at the end
     unsigned int buffer;
     //make a buffer and bind it for current use
     GLCall(glGenBuffers(1, &buffer));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
     //do not need to give data at initalisation, can be done latter neear use
     //GL_STATIC_DRAW as we will not be updating the triangle, but it is drawn a lot
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
     // index = 0 as first atribute is what we want to use, is the index used in the vertex shader
     // size = 2 as we have 2 floats in the position we want to use
     // type = GL_FLOAT
     // normalize = false as we are flaots 0<x<1
     // stride = 2 as each vertex only has two data points (x pos and y pos, could also have texture / normals ect n the sasme vertex)
     // offset in the vertex to the data we want (sat we wanted normal, use (const void*) 8, for example)
+    // this line is what binds the buffer to the vao
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
     // tell open gl that we are allowed to draw this buffer
+    // this allows us to ask to draw the vertexbuffer and index buffer again without spessifying it again
     GLCall(glEnableVertexAttribArray(0));
 
 
@@ -229,8 +240,16 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        //which shader will we use
+        GLCall(glUseProgram(shader));
         // change the unifrom before we draw the elements using the shader
         GLCall(glUniform4f(location, r, 0.0f, 1.0f - r, 1.0f));
+
+        // bind vao so we dont need to rebind the buffer and vertex pointer
+        GLCall(glBindVertexArray(vao));
+
+        // bind the index array to say which elelemnts in the vertex array we want to draw
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
         // draw ibo as it prevents duplicating vertex info in the gpu memory
         // what are we drawing, what index do we start, how many verts ect
